a63ce4a9575015b4c50cb37341df6183d2806b14
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-08-06 11:02:55 -0500

    full example working

--- ca.py
+++ ca.py
@@ -1,15 +1,9 @@
 import cellpylib as cpl
+import numpy as np
 
 
-cellular_automaton = cpl.init_random2d(600, 600)
-
-
-# evolve the cellular automaton for 60 time steps
-cellular_automaton = cpl.evolve2d(
-    cellular_automaton,
-    timesteps=60,
-    neighbourhood='Moore',
-    apply_rule=cpl.game_of_life_rule,
-)
-
-cpl.plot2d_animate(cellular_automaton)
+def fill_board(board: np.array, vals: list, x0: int, y0: int, w: int, h: int) -> np.array:
+    for i, x in enumerate(range(x0, x0 + w)):
+        for j, y in enumerate(range(y0, y0 + h)):
+            board[:, x, y] = vals[i*w + j]
+    return board
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from markov import new_markov_chain_graph, walk_markov_chain
 import networkx as nx
 import matplotlib.pyplot as plt
+import cellpylib as cpl
+from markov import new_markov_chain_graph, walk_markov_chain
+from ca import fill_board
 
 chain = new_markov_chain_graph([1, 0], [0.1, 0.2, 0.3, 0.4])
 nx.draw_networkx(chain)
 plt.savefig("filename.png")
 
 print(nx.attr_matrix(chain, edge_attr="w", rc_order=chain.nodes()))
-print([(n, nbrdict) for n, nbrdict in chain.adjacency()])
 
-initial_string = ""
-for i in walk_markov_chain(chain, steps=100, debug=True):
-    initial_string += str(i)
+initial_state = []
+for i in walk_markov_chain(chain, steps=100):
+    initial_state.append(i)
 
-print(initial_string)
+
+cellular_automaton = cpl.init_simple2d(100, 100)
+
+fill_board(cellular_automaton, initial_state, 5, 5, 10, 10)
+
+cellular_automaton = cpl.evolve2d(
+    cellular_automaton,
+    timesteps=100,
+    neighbourhood='Moore',
+    apply_rule=cpl.game_of_life_rule,
+)
+
+cpl.plot2d_animate(cellular_automaton)
